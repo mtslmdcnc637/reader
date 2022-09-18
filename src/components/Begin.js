3a5620835f39f39c53596d64b511f3b0c822c5a9
Mateus Lima <mtslmdcnc637@gmail.com>
2022-09-18 16:04:55 -0300

    v 3.2 - adicionado melhor visualização em celular

--- src/components/Begin.js
+++ src/components/Begin.js
@@ -1,5 +1,5 @@
 import style from './css/Begin.module.css'
-import {useState, useEfect} from 'react'
+import {useState, useEffect} from 'react'
 import DisplayReader from './DisplayReader';
 function Begin (){
     const [ppm, setPpm] = useState()
@@ -31,22 +31,26 @@ function Begin (){
                         2 - Escolha a velocidade de leitura - PPM (Palavras por minuto)
                     </li>
                     <li>
-                        3 - Se quiser pausar depois que a leiruracomeçar é so tocar no botão de pause
+                        3 - (em breve) - Se quiser pausar depois que a leirura começar é so tocar no botão de pause
+                    </li>
+                    <li>
+                        4 - (em breve)- Continue lendo textos anteriores de onde parou
                     </li>
                 </ul>
             
             
         <form className={style.form} onSubmit={getText}>
-            <input type="text" name="text" placeholder="Insira aqui seu texto"></input>
+            <input type="text" name="text" placeholder="Insira aqui seu texto" className={style.input}></input>
             <select name="ppm" id="ppm">
-                <option selected value="300" key="300">300 ppm</option>
+                <option value="250" key="250">250 ppm</option>
+                <option value="300" key="300" selected>300 ppm</option>
                 <option value="350" key="350">350 ppm</option>
                 <option value="400" key="400">400 ppm</option>
                 <option value="450" key="450">450 ppm</option>
                 <option value="500" key="500">500 ppm</option>
                 <option value="550" key="550">550 ppm</option>
             </select>
-            <input type="submit" value="Começe a ler"></input>
+            <input type="submit" value="Começe a ler" className={style.button}></input>
 
         </form>
         </div>
