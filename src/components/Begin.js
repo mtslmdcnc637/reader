79bb346071547d07e50a14df92a597f90cada9c5
Mateus Lima <mtslmdcnc637@gmail.com>
2022-10-23 16:47:59 -0300

    adicionado a opção de upload de pdfs

--- src/components/Begin.js
+++ src/components/Begin.js
@@ -5,6 +5,8 @@ import DisplayReader from './DisplayReader';
 import { AiFillCloseCircle } from "react-icons/ai";
 import DisplayPaused from './DisplayPaused';
 import icon from '../favicon.png'
+import api from './api';
+import axios from 'axios'
 
 function Begin (props){
     const [play, setPlay] = useState()
@@ -17,6 +19,10 @@ function Begin (props){
     const [load, setLoad] = useState()
     const [textState, setTextState] = useState()
     const [n, setN] = useState()
+    const getTextOnServer = document.getElementById("getTextOnServer");
+      const textarea = document.getElementById("textarea");
+      const inputBook = document.getElementById("inputBook");
+      const [pdf, setPdf] = useState('')
     useEffect(()=>{if(props.nWord){setN(parseInt(props.nWord))}else{setN(0)}},[])
     
     const [size, setSize] = useState()
@@ -28,7 +34,7 @@ function Begin (props){
         var getPpm = e.target.ppm.value
         setPpm(getPpm)
         setTextState(textStable) 
-
+        
         setTimeout(() => {
             setLoad()
             setPlay("play")
@@ -51,7 +57,26 @@ function Begin (props){
 
     }
     
-
+     async function uploadPdf(e) {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('pdf', pdf)
+        
+        await api.post("/pdf", formData)
+        .then((response) => {
+            const textarea = document.getElementById("textarea")
+            var text = JSON.stringify(response.data)
+            text = text.replace(/\\n/g, "")
+            text = text.trim()
+            textarea.value = text
+        }).catch((err) => {
+            if(err.response){
+                console.log(err.response);
+            }else{
+                console.log("ofline");
+            }
+        })
+     }
     return (
     <>
     
@@ -59,7 +84,7 @@ function Begin (props){
              <div className={style.body}>
              <div className={style.bg}>
                  <h5><img src={icon}></img>   Leitura Ninja</h5>
-                 <p>Bem-vindo! Leitura ninja é a mais nova ferramenta brasileira com objetivo acelerar a leitura de textos grandes. Você só precisa colar seu texto na área indicada, escolher a velocidade de leitura e apertar o play</p>
+                 <p>Bem-vindo! Leitura ninja é a mais nova ferramenta brasileira com objetivo de acelerar a leitura de textos grandes. Você só precisa colar seu texto na área indicada, escolher a velocidade de leitura e apertar o play</p>
                  <div className={style.buttons}>
                      <a href="https://pag.ae/7YHBnioxL" target="_blank">Ajude no desenvolvimento da ferramenta</a>
                  </div>
@@ -72,27 +97,31 @@ function Begin (props){
              </div>
              <div className={style.form_cont}>
                      <form className={style.form} onSubmit={getText} >
-                     <textarea name="text" placeholder="Insira aqui seu texto" value={props.text} className={style.input}></textarea>
+                     <textarea name="text" placeholder="Insira aqui seu texto" id="textarea" defaultValue={props.text} className={style.input}></textarea>
                      <div><span>Velocidade</span>
                      <select name="ppm" id="ppm">
-                         <option value="200" key="200">200 ppm</option>
+                         <option value="100" key="100">100 ppm - lento</option>
+                         <option value="150" key="150">150 ppm</option>
+                         <option value="200" key="200"selected>200 ppm</option>
                          <option value="250" key="250">250 ppm</option>
-                         <option value="300" key="300" selected>300 ppm</option>
+                         <option value="300" key="300" >300 ppm</option>
                          <option value="350" key="350">350 ppm</option>
                          <option value="400" key="400">400 ppm</option>
                          <option value="450" key="450">450 ppm</option>
                          <option value="500" key="500">500 ppm</option>
-                         <option value="550" key="550">550 ppm</option>
+                         <option value="550" key="550">550 ppm -rapido</option>
                      </select></div>
                      <select name="words" id="words">
                          <option value="1" key="1" selected>1 palavra de cada vez</option>
-                         
-                     </select>
-                     <input type="submit" value={"Começar a ler"} className={style.button}></input>
+                     </select><input type="submit" value="Começar a ler" className={style.button}></input>
  
-                 </form>  
+                 </form>
+                <form onSubmit={uploadPdf} className={style.subpdf}>
+                     <input type="file" name="pdf" id="inputBook" onChange={e =>{setPdf(e.target.files[0])}}/>
+                     <input type='submit' value="Processar PDF" id="getTextOnServer"/>
+                </form>  
              </div>
- 
+            
          </div>
          )}
         
@@ -117,8 +146,7 @@ function Begin (props){
             </div>
         </div>)
         }
-        
-        
+      
         
         </>
     )
---
+++ src/components/api.js
@@ -0,0 +1,4 @@
+import axios from 'axios'
+
+ const api = axios.create({baseURL: "https://leituraninja.mateuslima39.repl.co"});
+export default api
